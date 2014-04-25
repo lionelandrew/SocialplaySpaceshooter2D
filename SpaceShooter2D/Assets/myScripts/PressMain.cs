6abeab637121f2442978ba7aa5743c92238a25a5
Nicholas <nickmorais88@gmail.com>
2014-04-25 17:34:27 -0400

    Worked on inventory level.

---
+++ SpaceShooter2D/Assets/myScripts/PressMain.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressMain : MonoBehaviour {
+	
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.D))
+		{
+			Application.LoadLevel("Done_Main");
+		}
+	}
+
+}
