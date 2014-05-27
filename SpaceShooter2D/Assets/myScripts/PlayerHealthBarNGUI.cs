0845935aed5e02e2034316d49e678bc5f6542ab5
Geoffrey hale <666java@gmail.com>
2014-05-27 12:24:29 -0400

    player health bar on ngui in but working on fixing some problems still

---
+++ SpaceShooter2D/Assets/myScripts/PlayerHealthBarNGUI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealthBarNGUI : MonoBehaviour
+{
+    public UISlider _healthBar;
+    private float currentHealth;
+    private float maxHealth;
+    private float normalisedHealth;
+    private Characters script;
+    public GameObject healthBarForeground;
+
+    void Awake()
+    {
+        if (_healthBar != null)
+        {
+            _healthBar = GetComponent<UISlider>();
+        }
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            script = player.GetComponent<Characters>();
+        }
+    }
+
+    void Update()
+    {
+        currentHealth = script.currentHealth;
+        maxHealth = script.maxHealth;
+        normalisedHealth = ((float)currentHealth / maxHealth) * 1;
+        _healthBar.value = normalisedHealth;
+
+        if (_healthBar.value == 0)
+            healthBarForeground.SetActive(false);
+    }
+}
