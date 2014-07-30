db7b3009740eb22664ff874ee7a746da4f793a20
Nicholas <nickmorais88@gmail.com>
2014-07-30 17:56:31 -0400

    completed

--- SpaceShooter2D/Assets/myScripts/Done_PlayerController.cs
+++ SpaceShooter2D/Assets/myScripts/Done_PlayerController.cs
@@ -12,15 +12,19 @@ public class Done_PlayerController : MonoBehaviour
 	public float speed;
 	public float tilt;
 	public Done_Boundary boundary;
-
+	
 	public GameObject shot;
 	public Transform shotSpawn;
 	public float fireRate;
-	 
+	
+	public float accel;
+	public float decel;
+	public float maxSpeed;
+	public int damage = 0;
+	
+	private float time = 0;
 	private float nextFire;
-
-    public int damage = 0;
-
+	
 	void Update ()
 	{
 		if (Input.GetButton("Fire1") && Time.time > nextFire) 
@@ -30,22 +34,97 @@ public class Done_PlayerController : MonoBehaviour
 			audio.Play ();
 		}
 	}
-
+	
 	void FixedUpdate ()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		rigidbody.velocity = movement * speed;
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			time = Time.deltaTime;
+			speed = maxSpeed / (accel * time);
+			if(speed > maxSpeed)
+			{
+				speed = maxSpeed;
+				time = 0;
+			}
+			rigidbody.velocity = new Vector3 (-speed,0,0);
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			time = Time.deltaTime;
+			speed = maxSpeed / (accel * time);
+			if(speed > maxSpeed)
+			{
+				speed = maxSpeed;
+				time = 0;
+			}
+			
+			rigidbody.velocity = new Vector3 (speed,0,0);
+		}
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			time = Time.deltaTime;
+			speed = maxSpeed / (accel * time);
+			if(speed > maxSpeed)
+			{
+				speed = maxSpeed;
+				time = 0;
+			}
+			rigidbody.velocity = new Vector3 (0,0,speed);
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			time = Time.deltaTime;
+			speed = maxSpeed / (accel * time);
+			if(speed > maxSpeed)
+			{
+				speed = maxSpeed;
+				time = 0;
+			}
+			rigidbody.velocity = new Vector3 (0,0,-speed);
+		}
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			rigidbody.velocity = new Vector3 (0,0,0);
+		}
 		
-		rigidbody.position = new Vector3
-		(
-			Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax), 
-			0.0f, 
-			Mathf.Clamp (rigidbody.position.z, boundary.zMin, boundary.zMax)
-		);
+		//deceleration
+		if (Input.GetKeyUp(KeyCode.LeftArrow))
+		{
+			time = Time.deltaTime;
+			speed = decel - time;
+			
+			rigidbody.velocity = new Vector3 (-speed,0,0);
+		}
+		if (Input.GetKeyUp(KeyCode.RightArrow))
+		{
+			time = Time.deltaTime;
+			speed = decel - time;
+			
+			rigidbody.velocity = new Vector3 (speed,0,0);
+		}
+		if (Input.GetKeyUp(KeyCode.UpArrow))
+		{
+			time = Time.deltaTime;
+			speed = decel - time;
+			
+			rigidbody.velocity = new Vector3 (0,0,speed);
+		}
+		if (Input.GetKeyUp(KeyCode.DownArrow))
+		{
+			time = Time.deltaTime;
+			speed = decel - time;
+			
+			rigidbody.velocity = new Vector3 (0,0,-speed);
+		}
 		
+		time = 0;
 		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+		
+		//		rigidbody.position = new Vector3
+		//		(
+		//			Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax), 
+		//			0.0f, 
+		//			Mathf.Clamp (rigidbody.position.z, boundary.zMin, boundary.zMax)
+		//		);
 	}
 }
--- SpaceShooter2D/Assets/myScripts/StatusNGUI.cs
+++ SpaceShooter2D/Assets/myScripts/StatusNGUI.cs
@@ -3,72 +3,72 @@ using System.Collections;
 
 public class StatusNGUI : MonoBehaviour
 {
-
-    public UILabel Damage;
-    public UILabel AttackRate;
-    public UILabel Speed;
-    public UILabel Accelleration;
-    public UILabel Deceleration;
-    public UILabel ShieldStrength;
-    public UILabel Protection;
-
-    private float damage;
-    private float atkrate;
-    private float accelleration;
-    private float deceleration;
-    private float speed;
-    private float shield;
-    private float protection;
-
-    private Done_PlayerController script;
-
-    // Use this for initialization
-    void Start()
-    {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        if (player != null)
-        {
-            script = player.GetComponent<Done_PlayerController>();
-        }
-        UpdateStats();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        OnButtonPress();
-
-        Damage.text = "Damage: " + damage;
-        AttackRate.text = "Attack Rate: " + atkrate;
-        Accelleration.text = "Accelleration: " + accelleration;
-        Deceleration.text = "Deceleration: " + deceleration;
-        Speed.text = "Speed: " + speed;
-        ShieldStrength.text = "Shield Strength: " + shield;
-        Protection.text = "Protection: " + protection;
-    }
-
-    void OnButtonPress()
-    {
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            damage += 5;
-            atkrate += 5;
-            accelleration += 5;
-            deceleration += 5;
-            speed += 5;
-            shield += 10;
-            protection += 10;
-        }
-    }
-
-    void UpdateStats()
-    {
-        damage = 0;
-        atkrate = script.fireRate;
-        accelleration = 0;
-        deceleration = 0;
-        speed = script.speed;
-        shield = 0;
-        protection = 0;
-    }
+	
+	public UILabel Damage;
+	public UILabel AttackRate;
+	public UILabel Speed;
+	public UILabel Accelleration;
+	public UILabel Deceleration;
+	public UILabel ShieldStrength;
+	public UILabel Protection;
+	
+	private float damage;
+	private float atkrate;
+	private float accelleration;
+	private float deceleration;
+	private float speed;
+	private float shield;
+	private float protection;
+	
+	private Done_PlayerController script;
+	
+	// Use this for initialization
+	void Start()
+	{
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		if (player != null)
+		{
+			script = player.GetComponent<Done_PlayerController>();
+		}
+		UpdateStats();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		//OnButtonPress();
+		
+		Damage.text = "Damage: " + damage;
+		AttackRate.text = "Attack Rate: " + atkrate;
+		Accelleration.text = "Accelleration: " + accelleration;
+		Deceleration.text = "Deceleration: " + deceleration;
+		Speed.text = "Speed: " + speed;
+		ShieldStrength.text = "Shield Strength: " + shield;
+		Protection.text = "Protection: " + protection;
+	}
+	
+	//    void OnButtonPress()
+	//    {
+	//        if (Input.GetKeyDown(KeyCode.K))
+	//        {
+	//            damage += 5;
+	//            atkrate += 5;
+	//            accelleration += 5;
+	//            deceleration += 5;
+	//            speed += 5;
+	//            shield += 10;
+	//            protection += 10;
+	//        }
+	//    }
+	
+	void UpdateStats()
+	{
+		damage = script.damage;
+		atkrate = script.fireRate;
+		accelleration = script.accel;
+		deceleration = script.decel;
+		speed = script.speed;
+		shield = 0;
+		protection = 0;
+	}
 }
